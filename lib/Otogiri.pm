3aed7b64146232d10f887fdb2dca6d1862685e83
Satoshi Azuma <ytnobody@gmail.com>
2013-12-28 15:54:15 +0000

    Import of YTURTLE/Otogiri-0.05 from CPAN.
    gitpan-cpan-distribution: Otogiri
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         YTURTLE/Otogiri-0.05.tar.gz
    gitpan-cpan-author:       YTURTLE
    gitpan-cpan-maturity:     released

--- lib/Otogiri.pm
+++ lib/Otogiri.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 use Class::Accessor::Lite (
     ro => [qw/connect_info/],
@@ -38,7 +38,7 @@ sub _deflate_param {
 
 sub _inflate_rows {
     my ($self, $table, @rows) = @_;
-    @rows = $self->{inflate} ? map {$self->{inflate}->($_, $table)} @rows : @rows;
+    @rows = $self->{inflate} ? map {$self->{inflate}->($_, $table)} grep {defined $_} @rows : @rows;
     wantarray ? @rows : $rows[0];
 }
 
